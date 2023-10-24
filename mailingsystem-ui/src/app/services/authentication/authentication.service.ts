60b91fd5f0976032f1e397f333d063e62258f3d7
gongora84 <henry.gcolumbie@gmail.com>
2023-10-24 16:05:39 -0400

    moving generic dto to outside folder

--- mailingsystem-ui/src/app/services/authentication/authentication.service.ts
+++ mailingsystem-ui/src/app/services/authentication/authentication.service.ts
@@ -3,7 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {LoginDto} from "../../models/login/login-dto";
 import {map, Observable} from "rxjs";
 import {LoginResponse} from "../../models/login/login-response";
-import {ApiResponseModel} from "../../models/responses/generics/api-reponse-dtos";
+import {ApiResponseModel} from "../../models/api-reponse-dtos";
 
 @Injectable({
   providedIn: 'root',
